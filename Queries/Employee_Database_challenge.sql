c91e72de975793e348de2539667fa2566ea251b8
vanessamignelli <79024337+vanessamignelli@users.noreply.github.com>
2021-04-10 20:17:58 -0400

    adding retirement queries and data

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,60 @@
+-- find employee details from the employees detail
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+-- find title details from the titles table
+SELECT title, from_date, to_date
+FROM titles;
+
+-- create retirement titles table through employee and titles join
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- count titles in the unique titles table
+SELECT COUNT (title), title
+FROM unique_titles
+GROUP BY (title);
+
+-- create table to hold the unique titles info
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY (title)
+ORDER BY (COUNT (title)) DESC;
+
+-- create a mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
